460639d62c5b9be43e04a713b1b3e94ea5f0003a
Abhinaya S M <abhinayanandhini07@gmail.com>
2025-12-11 23:49:11 +0530

    km to m.java

---
+++ km to m.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner sc= new Scanner(System.in);
+	    Double km=sc.nextDouble();
+	     Double m=km*1000;
+	    System.out.println(m);
+        }
+     }

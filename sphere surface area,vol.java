d3806b94448657bbf86ad2beeddd6595ab3fabaf
Abhinaya S M <abhinayanandhini07@gmail.com>
2025-12-11 23:35:19 +0530

    sphere surface area,vol.java

---
+++ sphere surface area,vol.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner sc= new Scanner(System.in);
+	    double r= sc.nextDouble();
+	    double sa =4*3.14 *r*r ;
+        double v = (4.0/3.0) *3.14 *r*r*r ;
+        System.out.println(sa);
+        System.out.println(v);
+        }
+     }

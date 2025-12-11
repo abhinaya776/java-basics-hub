92f09b8d9e56e8b8b2bc81f727f3e1172833d848
Abhinaya S M <abhinayanandhini07@gmail.com>
2025-12-11 12:57:53 +0530

    except last digit.java

---
+++ except last digit.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner sc=new Scanner(System.in);
+	    int a=sc.nextInt();
+	    int digit=a/10;
+	    System.out.println(digit);
+	    
+		
+	}
+}

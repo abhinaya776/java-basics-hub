35c693ac049458937b70388f598c5fb9c5b786f3
Abhinaya S M <abhinayanandhini07@gmail.com>
2025-12-11 19:27:19 +0530

    circumference of circle.java

---
+++ circumference of circle.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner sc=new Scanner (System.in);
+		double r=sc.nextDouble();
+	    double peri= 2*3.14*r;
+	    System.out.println(peri);
+	 }
+}

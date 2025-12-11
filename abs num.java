a548fda5f4975cc2bbd623cfd33848f7c6d8ec85
Abhinaya S M <abhinayanandhini07@gmail.com>
2025-12-11 15:22:01 +0530

    abs num.java

---
+++ abs num.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner sc=new Scanner (System.in);
+		int a=sc.nextInt();
+	    int abs= (a<0)? -a : a;
+	    System.out.println(abs);
+	     
+	}
+}

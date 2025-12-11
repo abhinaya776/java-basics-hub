be4c89c354f689e13b33d0f80b427bfd21064188
Abhinaya S M <abhinayanandhini07@gmail.com>
2025-12-11 22:53:54 +0530

    distance btw 2 pts.java

---
+++ distance btw 2 pts.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner sc= new Scanner(System.in);
+	double x1=sc.nextDouble();
+	double x2=sc.nextDouble();
+	double y1=sc.nextDouble();
+	double y2=sc.nextDouble();
+	double dx= x2-x1;
+	double dy=y2-y1;
+	double dis=Math.sqrt((dx*dx)+(dy*dy));
+	System.out.println(dis);
+		}
+     }

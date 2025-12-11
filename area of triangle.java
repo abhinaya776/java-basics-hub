3fd9bc912d6565506c9dbeca419e136d499a5289
Abhinaya S M <abhinayanandhini07@gmail.com>
2025-12-11 19:08:59 +0530

    area of triangle.java

---
+++ area of triangle.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner sc=new Scanner (System.in);
+		double b=sc.nextInt();
+	    double h=sc.nextInt();
+	    System.out.println(0.5*b*h);
+	 }
+}

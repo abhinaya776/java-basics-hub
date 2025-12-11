6890b052e6d323e0cbc32a8493d4d535c329df0b
Abhinaya S M <abhinayanandhini07@gmail.com>
2025-12-11 23:40:55 +0530

    distance travelled.java

---
+++ distance travelled.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner sc= new Scanner(System.in);
+	    Double speed=sc.nextDouble();
+	    Double time=sc.nextDouble();
+	    Double dis=speed*time;
+	    System.out.println(dis);
+        }
+     }

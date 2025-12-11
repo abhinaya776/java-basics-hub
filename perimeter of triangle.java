c1bd39f545cccb2f49c9db6d610434ebedeae7af
Abhinaya S M <abhinayanandhini07@gmail.com>
2025-12-11 19:23:55 +0530

    perimeter of triangle.java

---
+++ perimeter of triangle.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner sc=new Scanner (System.in);
+		double s1=sc.nextDouble();
+		double s2=sc.nextDouble();
+		double s3=sc.nextDouble();
+		double peri=s1+s2+s3;
+	    System.out.println(peri);
+	 }
+}

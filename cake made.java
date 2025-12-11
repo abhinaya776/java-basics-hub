138e5656f737f978f12bebc7683e1d53c0433177
Abhinaya S M <abhinayanandhini07@gmail.com>
2025-12-12 00:30:07 +0530

    cake made.java

---
+++ cake made.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner sc= new Scanner(System.in);
+	    int cakeperhr=sc.nextInt();
+	     int numofhrs =sc.nextInt();
+	     int total=cakeperhr*numofhrs;
+	    System.out.println(total);
+	    }
+     }

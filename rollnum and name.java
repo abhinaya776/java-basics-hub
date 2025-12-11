c33fa64502c86f0fbaacba695586e33b1d72b52a
Abhinaya S M <abhinayanandhini07@gmail.com>
2025-12-11 22:11:44 +0530

    rollnum and name.java

---
+++ rollnum and name.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner sc= new Scanner(System.in);
+		int rollnum=sc.nextInt();
+		String str=sc.nextLine();
+		String name=sc.nextLine();
+		System.out.println("Rollnum: " +rollnum);
+		System.out.println("Name: " +name);
+		
+	}
+}

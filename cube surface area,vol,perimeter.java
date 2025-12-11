c5fb71f55d6754431cac4f0d7021b1944db79a65
Abhinaya S M <abhinayanandhini07@gmail.com>
2025-12-11 23:02:23 +0530

    cube surface area,vol,perimeter.java

---
+++ cube surface area,vol,perimeter.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner sc= new Scanner(System.in);
+	    double a= sc.nextDouble();
+	    double sa = 6 * a * a;
+        double v = a * a * a;
+        double p = 12 * a;
+        System.out.println(sa);
+        System.out.println(v);
+        System.out.println(p);
+        
+	
+		}
+     }

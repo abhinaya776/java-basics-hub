694f041be7a3e036dbd1e75385c6a1c46d84d64e
Abhinaya S M <abhinayanandhini07@gmail.com>
2025-12-11 19:21:09 +0530

    perimeter of rec.java

---
+++ perimeter of rec.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner sc=new Scanner (System.in);
+		double l=sc.nextDouble();
+		double b=sc.nextDouble();
+		double peri=2*(l+b);
+	    System.out.println(peri);
+	 }
+}

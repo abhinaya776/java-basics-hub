1de13f6741f516d8f6e2f9f7be18c966542bd3e4
Abhinaya S M <abhinayanandhini07@gmail.com>
2025-12-11 19:25:59 +0530

    perimeter of square.java

---
+++ perimeter of square.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner sc=new Scanner (System.in);
+		double s=sc.nextDouble();
+	    double peri= 4*s;
+	    System.out.println(peri);
+	 }
+}

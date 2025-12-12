afce2a8da87f9c0f67289999044d003fdb9dd6d9
Abhinaya S M <abhinayanandhini07@gmail.com>
2025-12-12 09:57:28 +0530

    SP given CP and Profit.java

---
+++ SP given CP and Profit.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner sc=new Scanner(System.in);
+	    Double cost=sc.nextDouble();
+	    Double profit=sc.nextDouble();
+	    Double sellingprice= cost+ profit;
+	    System.out.println(sellingprice);
+	}
+}

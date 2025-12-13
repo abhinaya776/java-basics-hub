dd47e31146a6bbe67768e1fb065a50fde3c4576c
Abhinaya S M <abhinayanandhini07@gmail.com>
2025-12-13 15:05:08 +0530

    Create year's first two and last two digits are same or not check.java

---
+++ year's first two and last two digits are same or not check.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String []args){
+        Scanner sc=new Scanner(System.in);
+        int year=sc.nextInt();
+        int first=year/100;
+        int last=year%100;
+       if (first==last){
+          System.out.println("same"); 
+       }
+       else {
+           System.out.println("diff");
+       }
+    }}

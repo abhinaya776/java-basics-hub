a18217acd33a0f40e0ae5e960c81cda5cd835e3e
Abhinaya S M <abhinayanandhini07@gmail.com>
2025-12-13 12:27:22 +0530

    Create divisible by 2 and 5 not by 8.java

---
+++ divisible by 2 and 5 not by 8.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String []args){
+        Scanner sc=new Scanner(System.in);
+        int num =sc.nextInt();
+        if(num%2==0 && num%5==0 && num%8!=0){
+            System.out.println("Divisible by 2 and 5 not by 8");
+            }
+        else
+          System.out.println("condition not satisfied");
+    }
+}

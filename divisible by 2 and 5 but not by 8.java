48b8dcaf67a9624aaf3f753ba546297dff614230
Abhinaya S M <abhinayanandhini07@gmail.com>
2025-12-13 12:04:29 +0530

    divisible by 2 and 5 but not by 8.java

--- divisible by 2 and 5 but not by 8.java
+++ divisible by 2 and 5 but not by 8.java
@@ -3,11 +3,9 @@ public class Main{
     public static void main(String []args){
         Scanner sc=new Scanner(System.in);
         int num=sc.nextInt();
-       if (num%2==0 && num%5==0){
-           if(num%8!=0){
-           System.out.println("Divisible by 2 or 5 and not by 8");
+       if (num%2==0 && num%5==0 && num%8!=0) {
+         System.out.println("Divisible by 2 or 5 and not by 8");
            }
-       }
        else
            System.out.println("Not divisible by 2 and 5");
 }

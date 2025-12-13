46fb3de70a9b7223bfd44a5417924ac05886748b
Abhinaya S M <abhinayanandhini07@gmail.com>
2025-12-13 15:15:37 +0530

    Create address in multiple lines.java

---
+++ address in multiple lines.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String []args){
+        Scanner sc=new Scanner(System.in);
+         System.out.println("Door no: 45 \n Street: church colony \ncity:Coimbatore");
+       
+       
+    }}

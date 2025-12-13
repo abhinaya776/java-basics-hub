89db51fc17cc8956cabac3223c1ee4063d5ad444
Abhinaya S M <abhinayanandhini07@gmail.com>
2025-12-13 14:14:08 +0530

    Create three sides form valid triangle or not.java

---
+++ three sides form valid triangle or not.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String []args){
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int c=sc.nextInt();
+       if ((a+b>c)&&(b+c>a)&&(a+c>b)){
+          System.out.println("Valid triangle"); 
+       }
+       else
+           System.out.println("Not valid");
+}
+}

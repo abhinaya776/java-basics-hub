1adc2d87e89fde0c99fbf05afcf789651f8fb4eb
Abhinaya S M <abhinayanandhini07@gmail.com>
2025-12-13 12:48:25 +0530

    Create right angled triangle check.java

---
+++ right angled triangle check.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String []args){
+        Scanner sc=new Scanner(System.in);
+        int a =sc.nextInt();
+        int b=sc.nextInt();
+        int c=sc.nextInt();
+        
+        if((a*a==b*b + c*c)|| (b*b==a*a + c*c)|| (c*c==a*a +b*b)){
+            System.out.println("Right angeled triangle");
+            }
+        else
+          System.out.println("Not a right angeled triangle");
+    }
+}

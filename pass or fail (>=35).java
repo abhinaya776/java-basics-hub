8cb308825266215467a5c3206f097a5f389f5765
Abhinaya S M <abhinayanandhini07@gmail.com>
2025-12-14 20:00:28 +0530

    Create pass or fail (>=35).java

---
+++ pass or fail (>=35).java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter marks: ");
+        int marks = sc.nextInt();
+
+        if (marks >= 35) {
+            System.out.println("Pass");
+        } else {
+            System.out.println("Fail");
+        }
+
+        sc.close();
+    }
+}

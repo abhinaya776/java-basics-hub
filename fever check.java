225ad923a3eab462fe4e26624209f3104fd91698
Abhinaya S M <abhinayanandhini07@gmail.com>
2025-12-14 19:57:04 +0530

    Create fever check.java

---
+++ fever check.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter body temperature: ");
+        double temp = sc.nextDouble();
+
+        if (temp > 100) {
+            System.out.println("Fever detected");
+        } else {
+            System.out.println("Normal temperature");
+        }
+
+        sc.close();
+    }
+}

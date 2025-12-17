0cea7a4d9091d9c76c9b02402993e7a90c6d8532
Abhinaya S M <abhinayanandhini07@gmail.com>
2025-12-17 20:04:46 +0530

    Create whole numbers till N.java

---
+++ loops/whole numbers till N.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter N:");
+        int n=sc.nextInt();
+        for(int i=0;i<=n;i++){
+            System.out.print(i+ " ");
+        }
+    }
+}
